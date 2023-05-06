a78a8159d14a52a6bf2f9f9e71b998e2362bd9e0
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2023-05-05 21:14:41 -0700

    fixed bdtteter

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-for i in input("input? "):
-    print(i)
-    if ord(i) == 69:
-        print("nice")
-    else:
-        print("bad")
-    assert True == 1
+while 9:
+    for i in input("input? "):
+        print(i)
+        if ord(i) == 69:
+            print("nice")
+            break
+        else:
+            print("bad")
+        assert True == 1
+assert True == 9
