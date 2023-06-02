9e32e7a135f6fc416393d3fd59b5c962162764a7
hellopir2 <104927802+hellopir2@users.noreply.github.com>
2023-06-02 00:23:49 -0700

    Update binsqrt.py

--- binsqrt.py
+++ binsqrt.py
@@ -12,4 +12,15 @@ def binsqrt(low, high, n, power):
             return binsqrt(mid + 1, high, n, power)
     else:
         return -1
-
+#inaccurate mode
+def binsqrt(low, high, n, power):
+    if high >= low:
+        mid = (high + low) // 2
+        if mid ** power == n:
+            return mid
+        elif mid ** power > n:
+            return binsqrt(low, mid - 1, n, power)
+        else:
+            return binsqrt(mid + 1, high, n, power)
+    else:
+        return low
